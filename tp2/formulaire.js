4e9637725d2c4c51bcb13c47d14bd6d6c5e16729
ngochungly <ngochung.ly@edu.univ-paris13.fr>
2021-10-23 14:23:39 +0200

    Ajouter validation pour mail

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -10,19 +10,35 @@ function validation() {
     let adress = document.forms["formPersonne"]["adress"].value;
     let mail = document.forms["formPersonne"]["mail"].value;
 
-    const champs = [nom, prenom, adress, mail];
+    const champs = [nom, prenom, adress];
     let lengthCheckResult = champs.every(lengthCheck);
+    let emailCheckResult = validateEmail(mail);
 
-    if (lengthCheckResult) {
+    if (lengthCheckResult && emailCheckResult) {
         document.getElementById("error").style.display = "none";
         document.getElementById("resultat").style.display = "block";
-        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value + " !";
+    } else if (lengthCheckResult && !emailCheckResult) {
+        document.getElementById("resultat").style.display = "none";
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").innerHTML = "L'adress mail est incorrecte.";
+    } else if (!lengthCheckResult && emailCheckResult) {
+        document.getElementById("resultat").style.display = "none";
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").innerHTML = "Les champs Nom, Prenom et Adress sont remplis et contiennent au moins 5 caractères.";
     } else {
         document.getElementById("resultat").style.display = "none";
         document.getElementById("error").style.display = "block";
+        document.getElementById("error").innerHTML = "L'adress mail est incorrecte. Les champs Nom, Prénom et Adress sont remplis et contiennent au moins 5 caractères.";
     }
+
 }
 
 function lengthCheck(value, index, array) {
     return value.length >= 5;
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
